5669e6b6747eaad1e71e6c30d0543bbd1a3a1a7e
ViDem02 <43616078+ViDem02@users.noreply.github.com>
2025-11-01 14:02:21 +0100

    now, cones.pcd is correctly positioned in the reference frame and visualized

---
+++ RD/2-second_prj_setup/matrix_transform.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+// This function displays the help
+void
+showHelp(const char *program_name)
+{
+    std::cout << std::endl;
+    std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+    std::cout << "-h:  Show this help." << std::endl;
+}
+
+// This is the main function
+int
+main(const int argc, char **argv)
+{
+    // Show help
+    if (pcl::console::find_switch(argc, argv, "-h") ||
+        pcl::console::find_switch(argc, argv, "--help"))
+    {
+        showHelp(argv[0]);
+        return 0;
+    }
+
+    // Fetch point cloud filename in arguments | Works with PCD and PLY files
+    bool file_is_pcd = false;
+
+    std::vector<int> filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
+
+
+    if (filenames.size() != 1)
+    {
+        filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
+
+        if (filenames.size() != 1)
+        {
+            showHelp(argv[0]);
+            return -1;
+        } else
+        {
+            file_is_pcd = true;
+        }
+    }
+
+    // Load file | Works with PCD and PLY files
+    pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+
+    if (file_is_pcd)
+    {
+        if (pcl::io::loadPCDFile(argv[filenames[0]], *source_cloud) < 0)
+        {
+            std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+            showHelp(argv[0]);
+            return -1;
+        }
+    } else
+    {
+        if (pcl::io::loadPLYFile(argv[filenames[0]], *source_cloud) < 0)
+        {
+            std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+            showHelp(argv[0]);
+            return -1;
+        }
+    }
+
+
+
+    // Rotation
+    Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+
+    transform_2.translation() << 0.0, 0.0, 0.0;
+    transform_2.rotate(Eigen::AngleAxisf(-M_PI/2, Eigen::Vector3f::UnitX()));
+    transform_2.rotate(Eigen::AngleAxisf(M_PI/2, Eigen::Vector3f::UnitZ()));
+
+    // Print the transformation
+    printf("\nMethod #2: using an Affine3f\n");
+    std::cout << transform_2.matrix() << std::endl;
+
+    // Do transform
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);
+
+
+    pcl::visualization::PCLVisualizer viewer("Matrix transformation example");
+
+    const pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler(
+        transformed_cloud, 230, 20, 20); // Red
+
+
+    viewer.addPointCloud(transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+
+
+    viewer.addCoordinateSystem(1.0, "cloud", 0);
+    viewer.setBackgroundColor(0.05, 0.05, 0.05, 0);
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "transformed_cloud");
+    viewer.setPosition(0, 0);
+    //viewer.initCameraParameters();
+
+    while (!viewer.wasStopped())
+    {
+        viewer.spinOnce(); // Display the visualizer until the 'q' key is pressed
+    }
+
+    return 0;
+}
